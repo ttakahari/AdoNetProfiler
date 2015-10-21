ef9bd34d11cf1810c683fd1c8f354b9dba196842
t.takahari <t.takahari@gloops.com>
2015-10-21 15:37:06 +0900

    Add the lock extension class.

---
+++ src/AdoNetProfiler/AdoNetProfiler/ReaderWriterLockSlimExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+namespace AdoNetProfiler
+{
+    public static class ReaderWriterLockSlimExtensions
+    {
+        public static void ExecuteWithReadLock(this ReaderWriterLockSlim readerWriterLockSlim, Action action)
+        {
+            readerWriterLockSlim.EnterReadLock();
+
+            action();
+
+            readerWriterLockSlim.ExitReadLock();
+        }
+
+        public static T ExecuteWithReadLock<T>(this ReaderWriterLockSlim readerWriterLockSlim, Func<T> action)
+        {
+            readerWriterLockSlim.EnterReadLock();
+
+            var result = action();
+
+            readerWriterLockSlim.ExitReadLock();
+
+            return result;
+        }
+
+        public static void ExecuteWithWriteLock(this ReaderWriterLockSlim readerWriterLockSlim, Action action)
+        {
+            readerWriterLockSlim.EnterWriteLock();
+
+            action();
+
+            readerWriterLockSlim.ExitWriteLock();
+        }
+
+        public static T ExecuteWithWriteLock<T>(this ReaderWriterLockSlim readerWriterLockSlim, Func<T> action)
+        {
+            readerWriterLockSlim.EnterWriteLock();
+
+            var result = action();
+
+            readerWriterLockSlim.ExitWriteLock();
+
+            return result;
+        }
+    }
+}
