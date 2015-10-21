282f455e6d89884e3987dc3c7b12e2239f5843b9
t.takahari <t.takahari@gloops.com>
2015-10-21 15:08:45 +0900

    Add the wrapped transaction class.

---
+++ src/AdoNetProfiler/AdoNetProfiler/ProfiledDbTransaction.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+
+namespace AdoNetProfiler
+{
+    public class ProfiledDbTransaction : DbTransaction
+    {
+        private DbConnection _connection;
+        private DbTransaction _transaction;
+        private readonly IProfiler _profiler;
+
+        protected override DbConnection DbConnection => _connection;
+
+        public override IsolationLevel IsolationLevel => _transaction.IsolationLevel;
+
+        internal DbTransaction WrappedDbTransaction => _transaction;
+
+        public ProfiledDbTransaction(DbTransaction transaction, DbConnection connection, IProfiler profiler)
+        {
+            if (transaction == null)
+                throw new ArgumentNullException(nameof(transaction));
+
+            if (connection == null)
+                throw new ArgumentNullException(nameof(connection));
+
+            _transaction = transaction;
+            _connection  = connection;
+            _profiler    = profiler;
+        }
+
+        public override void Commit()
+        {
+            if (_profiler == null || !_profiler.IsEnabled)
+            {
+                _transaction.Commit();
+                return;
+            }
+
+            _profiler.OnTransactionCommit(() => _transaction.Commit());
+        }
+
+        public override void Rollback()
+        {
+            if (_profiler == null || !_profiler.IsEnabled)
+            {
+                _transaction.Rollback();
+                return;
+            }
+
+            _profiler.OnTransactionRollback(() => _transaction.Rollback());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                _transaction?.Dispose();
+
+            _transaction = null;
+            _connection  = null;
+
+            base.Dispose(disposing);
+        }
+    }
+}
