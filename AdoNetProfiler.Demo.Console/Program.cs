1c9fb03acd92553a0148b2547ead0966fc3051b1
t.takahari <t.takahari@gloops.com>
2015-12-21 11:20:40 +0900

    Add ConsoleApp Demo.

---
+++ AdoNetProfiler.Demo.Console/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Configuration;
+using System.Data.Common;
+
+namespace AdoNetProfiler.Demo.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdoNetProfilerFactory.Initialize(typeof(TraceProfiler));
+
+            var settings = ConfigurationManager.ConnectionStrings["Northwind"];
+            var factory = DbProviderFactories.GetFactory(settings.ProviderName);
+
+            using (var connection = factory.CreateConnection())
+            {
+                connection.ConnectionString = settings.ConnectionString;
+
+                connection.Open();
+
+                using (var transaction = connection.BeginTransaction())
+                {
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = @"SELECT * FROM Orders";
+                        command.Transaction = transaction;
+
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                            }
+                        }
+                    }
+
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = @"UPDATE Orders SET OrderDate = @Now WHERE OrderID = @OrderId";
+
+                        var parameter1 = command.CreateParameter();
+                        parameter1.ParameterName = "@orderId";
+                        parameter1.Value = 10245;
+
+                        var parameter2 = command.CreateParameter();
+                        parameter2.ParameterName = "@now";
+                        parameter2.Value = DateTime.Now;
+
+                        command.Parameters.AddRange(new[] { parameter1, parameter2 });
+                        command.Transaction = transaction;
+
+                        command.ExecuteNonQuery();
+                    }
+                    
+                    transaction.Commit();
+                }
+            }
+        }
+    }
+}
---
+++ AdoNetProfiler.Demo.Console/TraceProfiler.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Diagnostics;
+
+namespace AdoNetProfiler.Demo.Console
+{
+    public class TraceProfiler : IAdoNetProfiler
+    {
+        private Stopwatch _stopwatch;
+
+        public bool IsEnabled => true;
+
+        private Stopwatch _connectionStopwatch;
+
+        public void OnOpening(DbConnection connection)
+        {
+            _stopwatch = Stopwatch.StartNew();
+            _connectionStopwatch = Stopwatch.StartNew();
+        }
+
+        public void OnOpened(DbConnection connection)
+        {
+            _stopwatch.Stop();
+            Trace.WriteLine($"Connection Open Duration - {_stopwatch.Elapsed.TotalMilliseconds} ms");
+        }
+
+        public void OnClosing(DbConnection connection)
+        {
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        public void OnClosed(DbConnection connection)
+        {
+            _stopwatch.Stop();
+            _connectionStopwatch.Stop();
+            Trace.WriteLine($"Connection Close Duration - {_stopwatch.Elapsed.TotalMilliseconds} ms");
+            Trace.WriteLine($"Connection Lifetime - {_connectionStopwatch.Elapsed.TotalMilliseconds} ms");
+        }
+
+        private Stopwatch _transactionStopwatch;
+
+        public void OnStartingTransaction(DbConnection connection)
+        {
+            _stopwatch = Stopwatch.StartNew();
+            _transactionStopwatch = Stopwatch.StartNew();
+        }
+
+        public void OnStartedTransaction(DbTransaction transaction)
+        {
+            _stopwatch.Stop();
+            Trace.WriteLine($"Transaction Start Duration - {_stopwatch.Elapsed.TotalMilliseconds} ms");
+        }
+
+        public void OnCommitting(DbTransaction transaction)
+        {
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        public void OnCommitted(DbConnection connection)
+        {
+            _stopwatch.Stop();
+            _transactionStopwatch.Stop();
+            Trace.WriteLine($"Transaction Commit Duration - {_stopwatch.Elapsed.TotalMilliseconds} ms");
+            Trace.WriteLine($"Transaction Lifetime - {_transactionStopwatch.Elapsed.TotalMilliseconds} ms");
+        }
+
+        public void OnRollbacking(DbTransaction transaction)
+        {
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        public void OnRollbacked(DbConnection connection)
+        {
+            _stopwatch.Stop();
+            _transactionStopwatch.Stop();
+            Trace.WriteLine($"Transaction Rollback Duration - {_stopwatch.Elapsed.TotalMilliseconds} ms");
+            Trace.WriteLine($"Transaction Lifetime - {_transactionStopwatch.Elapsed.TotalMilliseconds} ms");
+        }
+
+        private IDbCommand _command;
+
+        public void OnCommandStart(IDbCommand command)
+        {
+            _command = command;
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        public void OnCommandFinish(IDbCommand command, bool isReader)
+        {
+            if (!isReader)
+            {
+                _stopwatch.Stop();
+                Trace.WriteLine($"Command Info - Command : {_command.CommandText}, Duration {_stopwatch.Elapsed.TotalMilliseconds} ms");
+            }
+        }
+
+        public void OnReaderFinish(IDataReader reader)
+        {
+            _stopwatch.Stop();
+            Trace.WriteLine($"Command Info - Command : {_command.CommandText}, Duration {_stopwatch.Elapsed.TotalMilliseconds} ms");
+        }
+
+        public void OnCommandError(IDbCommand command, Exception exception)
+        {
+        }
+    }
+}
